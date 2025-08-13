2dc982f258b799f83e386e5c91fae9e6abc331f8
Zebaro <denok100100@gmail.com>
2025-08-13 22:51:07 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+PORT = 8000
+
+class Handler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Hello from CD test!\n")
+
+if __name__ == "__main__":
+    print(f"Serving on port {PORT}...")
+    HTTPServer(("0.0.0.0", PORT), Handler).serve_forever()
