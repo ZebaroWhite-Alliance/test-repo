acd8247ce4e3adc3fb1b5898f679101a4631a778
Itsko Dmitro <100050383+WhiteDh@users.noreply.github.com>
2025-08-14 00:29:41 +0300

    Update app.py
    fsds

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ class Handler(SimpleHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
-        self.wfile.write(b"Hello from CD test! Dima loh!\n")
+        self.wfile.write(b"Hello from CD test! Denis loh!\n")
 
 if __name__ == "__main__":
     print(f"Serving on port {PORT}...")
